dc304cf540d89ebe287bf662f8e193ab9e922e7b
Renan <renan.fachin@hotmail.com>
2022-12-16 18:56:28 -0300

    connection made with graphcms and returning data from API

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,5 +1,42 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_LESSONS_QUERY = gql`
+  query{
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+
+interface LessonProps {
+  id: string;
+  title: string;
+}
+
 export function App() {
+  // Buscando do jeito tradicional os dados da API
+  // useEffect(() => {
+  //   client.query({
+  //     query: GET_LESSONS_QUERY
+  //   }).then(response => {console.log(response.data)})
+  // }, [])
+
+  const { data } = useQuery<{lessons: LessonProps[]}>(GET_LESSONS_QUERY)
+  console.log(data)
+
   return (
-    <h1 className="">Hello</h1>
+    <>
+      <ul>
+        {data?.lessons.map((lesson) => {
+          return (
+            <li key={lesson.id} className="text-bold text-3xl text-violet-700">
+              {lesson.title}
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
--- event-platform/src/main.tsx
+++ event-platform/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Páginas
 import { App } from './App'
+
+// Estilos globais (tailwindcss)
 import './styles/global.css'
+
+// Conexão com API
+import { ApolloProvider } from '@apollo/client' // Context Provider
+import { client } from './lib/apollo'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
