52d76702a69c33d95428b478de78f150dd34d3c4
Renan <renan.fachin@hotmail.com>
2022-12-19 17:22:38 -0300

    fetching the API data and stepping through to create the components

--- event-platform/src/components/Sidebar.tsx
+++ event-platform/src/components/Sidebar.tsx
@@ -1,6 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
+const GET_LESSONS_QUERY = gql `
+    query {
+        lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+            id
+            lessonType
+            availableAt
+            title
+            slug
+        }
+    }
+`
+
+// Tipando os retornos da APi
+interface GetLessonsQueryResponse {
+    lessons: {
+        id: string;
+        title: string;
+        slug: string;
+        availableAt: string;
+        lessonType: 'live' | 'class'
+    }[]
+}
+
 export function Sidebar() {
+    // Aplicando a tipagem criada
+    const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+
     return(
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
             
@@ -9,12 +36,17 @@ export function Sidebar() {
             </span>
 
             <div className="flex flex-col gap-8">
-                <Lesson 
-                    title="Aula 01"
-                    slug="aula-01"
-                    avaibleAt={new Date()}
-                    type="class"
-                />
+                {data?.lessons.map(lesson => {
+                    return(
+                        <Lesson 
+                            key={lesson.id}
+                            title={lesson.title}
+                            slug={lesson.slug}
+                            avaibleAt={new Date(lesson.availableAt)}
+                            type={lesson.lessonType}
+                        />
+                    )
+                })}
             </div>
         </aside>
     )
