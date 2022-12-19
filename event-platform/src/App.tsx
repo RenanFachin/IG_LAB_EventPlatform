a12ae3f237d22a28b7715911822e0cf41b06f1fa
Renan <renan.fachin@hotmail.com>
2022-12-19 15:17:17 -0300

    Creates header and Logo component. In addition to creating custom tailwincss base colors

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,44 +1,9 @@
-import { gql, useQuery } from '@apollo/client'
+import { Event } from "./pages/Event";
 
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-
-interface LessonProps {
-  id: string;
-  title: string;
-}
 
 export function App() {
-  // Buscando do jeito tradicional os dados da API
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(response => {console.log(response.data)})
-  // }, [])
-
-  const { data } = useQuery<{lessons: LessonProps[]}>(GET_LESSONS_QUERY)
 
   return (
-    <>
-      <ul>
-        {data?.lessons.map((lesson) => {
-          return (
-            <li 
-            key={lesson.id} 
-            className="text-bold text-3xl text-violet-700"
-            >
-              {lesson.title}
-            </li>
-          )
-        })}
-      </ul>
-    </>
+    <Event />
   )
 }
