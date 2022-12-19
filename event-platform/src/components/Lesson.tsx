509df6553347bf25ed802ab57d928ab134caa5e6
Renan <renan.fachin@hotmail.com>
2022-12-19 17:38:48 -0300

    text formatting with date-fns

--- event-platform/src/components/Lesson.tsx
+++ event-platform/src/components/Lesson.tsx
@@ -1,5 +1,6 @@
 import { CheckCircle, Lock } from 'phosphor-react'
-import { isPast } from 'date-fns'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 interface LessonProps{
     title: string;
@@ -12,11 +13,12 @@ interface LessonProps{
 export function Lesson(props: LessonProps) {
 
     const isLessonAvailable = isPast(props.avaibleAt)
+    const availableDateFormatted = format(props.avaibleAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", { locale: ptBR })
 
     return(
         <a href="#">
             <span className="text-gray-300">
-               {props.avaibleAt.toString()}
+               {availableDateFormatted}
             </span>
 
             <div className="rounded border border-gray-500 p-4 mt-2">
